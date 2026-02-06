108cabe0faab1221b3ef2f1217d4ff85411da172
cfaisne <k2hz8ghpsh@privaterelay.appleid.com>
2026-02-06 14:05:14 -0500

    deleteddups

--- app/about 3/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function AboutPage() {
-  return (
-    <div style={{ padding: '100px 20px', textAlign: 'center' }}>
-      <h1>About Page</h1>
-      <p>Coming soon - will be converted from about.html</p>
-      <a href="/" style={{ color: '#B8A078' }}>← Back to Home</a>
-    </div>
-  )
-}
