fbca65e70ffaf2e06caf7b53b4e36ff653bb013c
cfaisne <k2hz8ghpsh@privaterelay.appleid.com>
2026-02-07 17:37:04 -0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,8 @@ import Link from 'next/link'
 import Image from 'next/image'
 import RequestAccessModal from './components/RequestAccessModal'
 
+export const dynamic = 'force-dynamic'
+
 export default function HomePage() {
 const searchParams = useSearchParams()
 const router = useRouter()
